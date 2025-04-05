c1e8a54cbd4af044a40d765c624f946017febbb7
Taylor Martin <taylormartind@gmail.com>
2025-04-05 15:05:02 -0400

    added create account btn to sign in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import { DashboardComponent } from './dashboard/dashboard.component'
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'login-component', pathMatch: 'full' },
 	{ path: 'login-component', component: LoginComponent },
 	{ path: 'signup-component', component: SignupComponent },
 	{ path: 'employee-component', component: EmployeeComponent },
-	{ path: 'dashboard-component', component: DashboardComponent },
 ];
