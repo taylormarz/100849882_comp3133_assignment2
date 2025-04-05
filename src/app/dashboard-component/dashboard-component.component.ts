154bc6f021bc285913fc15b208e8231922d03ab7
Taylor Martin <taylormartind@gmail.com>
2025-04-05 13:45:40 -0400

    added dashboard component

---
+++ src/app/dashboard-component/dashboard-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-component',
+  imports: [],
+  templateUrl: './dashboard-component.component.html',
+  styleUrl: './dashboard-component.component.css'
+})
+export class DashboardComponentComponent {
+
+}
