d34688ec534ec311d68d014afaf613aca7ee7671
Taylor Martin <taylormartind@gmail.com>
2025-04-05 14:18:49 -0400

    changed dashboard component naming

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard-component/dashboard-component.component'
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'login-component', pathMatch: 'full' },
