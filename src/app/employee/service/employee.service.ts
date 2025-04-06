fc141e1934287c792504686c01a845dfed902f8e
Taylor Martin <taylormartind@gmail.com>
2025-04-06 19:03:54 -0400

    updated employee service with api calls

--- src/app/employee/service/employee.service.ts
+++ src/app/employee/service/employee.service.ts
@@ -1,9 +1,87 @@
 import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { gql } from 'apollo-angular';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  constructor(private apollo: Apollo) {}
 
-  constructor() { }
+  getEmp(): Observable<any> {
+    return this.apollo.query({
+      query: gql`
+        query {
+          getEmployees {
+            id
+            firstName
+            lastName
+            employeeId
+          }
+        }
+      `
+    });
+  }
+
+  getEmpById(id: string): Observable<any> {
+    return this.apollo.query({
+      query: gql`
+        query($id: String!) {
+          getEmployee(id: $id) {
+            id
+            firstName
+            lastName
+            employeeId
+          }
+        }
+      `,
+      variables: { id }
+    });
+  }
+
+  addEmp(employee: any): Observable<any> {
+    return this.apollo.mutate({
+      mutation: gql`
+        mutation($firstName: String!, $lastName: String!, $employeeId: String!) {
+          addEmployee(firstName: $firstName, lastName: $lastName, employeeId: $employeeId) {
+            id
+            firstName
+            lastName
+            employeeId
+          }
+        }
+      `,
+      variables: employee
+    });
+  }
+
+  updateEmp(id: string, employee: any): Observable<any> {
+    return this.apollo.mutate({
+      mutation: gql`
+        mutation($id: String!, $firstName: String!, $lastName: String!, $employeeId: String!) {
+          updateEmployee(id: $id, firstName: $firstName, lastName: $lastName, employeeId: $employeeId) {
+            id
+            firstName
+            lastName
+            employeeId
+          }
+        }
+      `,
+      variables: { id, ...employee }
+    });
+  }
+
+  deleteEmp(id: string): Observable<any> {
+    return this.apollo.mutate({
+      mutation: gql`
+        mutation($id: String!) {
+          deleteEmployee(id: $id) {
+            id
+          }
+        }
+      `,
+      variables: { id }
+    });
+  }
 }
