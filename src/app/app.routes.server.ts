488f28729b6527b09b0f1637d61a2006f7ac5702
Taylor Martin <taylormartind@gmail.com>
2025-04-07 14:33:12 -0400

    moved route paths to app.route.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,13 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'login-component', renderMode: RenderMode.Prerender },
+  { path: 'signup-component', renderMode: RenderMode.Prerender },
+  { path: 'employee-component', renderMode: RenderMode.Server },
+  { path: 'add-employee', renderMode: RenderMode.Server },
+  { path: 'edit-employee/:id', renderMode: RenderMode.Server },
+  { path: 'view-employee/:id', renderMode: RenderMode.Server },
+  { path: 'delete-employee/:id', renderMode: RenderMode.Server },
+  { path: '**', renderMode: RenderMode.Client }
 ];
